ce039e078262728a17ccd0c33bf3f958ead0120d
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-25 00:48:23 +0600

    car parts added

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uqgvh.mongodb.net/?retryWrites=true&w=majority"`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partCollection = client.db('car_part').collection('parts');
+
+
+        app.get('/part', async (req, res) => {
+            const query = {};
+            const cursor = partCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 
 

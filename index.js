f5c299d2732953d69cbb955c2c6c059c71664043
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-25 22:53:45 +0600

    getting purchase id added

--- index.js
+++ index.js
@@ -47,6 +47,20 @@ async function run() {
             res.send(purchase);
         });
 
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await purchaseCollection.findOne(query);
+            res.send(product);
+        });
+
+        app.delete('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 

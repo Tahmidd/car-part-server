e182f636223f716f15ccb521b6f6c0e538093e4b
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-26 03:20:00 +0600

    admin role update added

--- index.js
+++ index.js
@@ -64,6 +64,16 @@ async function run() {
             res.send(users);
         });
 
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
 
         app.get('/purchase', verifyJWT, async (req, res) => {
             const client = req.query.client;

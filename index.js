79b8d2e40f62f4546d620e0d959c56fae40870aa
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-26 03:38:58 +0600

    getting admin role added

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        });
+
 
         app.get('/purchase', verifyJWT, async (req, res) => {
             const client = req.query.client;
